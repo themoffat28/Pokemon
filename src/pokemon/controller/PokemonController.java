b904f3f55e46cb7ccc53550122041c539b0d6827
themoffat28 <bthemoffat@gmail.com>
2016-12-07 09:52:59 -0700

    Finished Pokemon.Controller (need to fix errors through inheritance).

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -27,7 +27,39 @@ public class PokemonController
 	{
 		pokedex.add(new GeoDude("Hands with a face"));
 		pokedex.add(new GoGoat("Leaf Dog"));
-		pokedex.add(new Ponyta)
+		pokedex.add(new Ponyta("Pony that stepped on a wire"));
+		pokedex.add(new Rotom("Flying Candle"));
+		pokedex.add(new Staryu("Metal Starfish"));
+	}
+	
+	public String[] buildPokedexText()
+	{
+		String [] pokemonNames = new String[pokedex.size()];
+		for(int index = 0; index < pokedex.size(); index++)
+		{
+			pokemonNames[index] = pokedex.get(index).getName();
+		}
+		
+		return pokemonNames;
+	}
+	
+	public void updateSelected(int index, String name, int combat, int health, double speed)
+	{
+		Pokemon current = pokedex.get(index);
+		current.setName(name);
+		current.setAttackPoints(combat);
+		current.setSpeed(speed);
+		current.setHitPoints(health);
+	}
+	
+	public ArrayList<Pokemon> getPokedex()
+	{
+		return pokedex;
+	}
+	
+	public PokemonFrame getBaseFrame()
+	{
+		return pokemonFrame;
 	}
 }
 	
